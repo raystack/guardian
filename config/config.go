84c11fba494b15afd6237ff64f28266a77941569
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-02-11 11:24:13 +0700

    feat: add config package to read app configuration

---
+++ config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	// PortKey is the key for port configuration
+	PortKey = "PORT"
+	// DBHostKey is the key for database host configuration
+	DBHostKey = "DB_HOST"
+	// DBUserKey is the key for database user configuration
+	DBUserKey = "DB_USER"
+	// DBPasswordKey is the key for database password configuration
+	DBPasswordKey = "DB_PASSWORD"
+	// DBNameKey is the key for database name configuration
+	DBNameKey = "DB_NAME"
+	// DBPortKey is the key for database port configuration
+	DBPortKey = "DB_PORT"
+	// DBSslModeKey is the key for database ssl mode configuration
+	DBSslModeKey = "DB_SSLMODE"
+)
+
+// Config contains the application configuration
+type Config struct {
+	Port int
+
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	DBSslMode  string
+}
+
+// Load returns application configuration
+func Load() *Config {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./")
+	viper.AddConfigPath("../")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error reading config: %s", err))
+	}
+
+	viper.SetDefault(PortKey, 3000)
+	viper.SetDefault(DBSslModeKey, "disable")
+
+	return &Config{
+		Port:       viper.GetInt(PortKey),
+		DBHost:     viper.GetString(DBHostKey),
+		DBUser:     viper.GetString(DBUserKey),
+		DBPassword: viper.GetString(DBPasswordKey),
+		DBName:     viper.GetString(DBNameKey),
+		DBPort:     viper.GetString(DBPortKey),
+		DBSslMode:  viper.GetString(DBSslModeKey),
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/odpf/guardian
 
 go 1.15
 
-require gorm.io/gorm v1.20.12
+require (
+	github.com/spf13/viper v1.7.1
+	gorm.io/gorm v1.20.12
+)
