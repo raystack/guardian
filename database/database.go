261d88094762da06d6c89b99cfabf5c64ed10a44
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-02-11 11:31:19 +0700

    feat: create db package for establishing connection to db

---
+++ database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Config for database connection
+type Config struct {
+	Host     string
+	User     string
+	Password string
+	Name     string
+	Port     string
+	SslMode  string
+}
+
+// New returns the database instance
+func New(c *Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s dbname=%s password=%s port=%s sslmode=%s",
+		c.Host,
+		c.User,
+		c.Name,
+		c.Password,
+		c.Port,
+		c.SslMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return db, err
+}
+
+// Migrate auto migrate models
+func Migrate(db *gorm.DB, models ...interface{}) error {
+	return db.AutoMigrate(models...)
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/spf13/viper v1.7.1
+	gorm.io/driver/postgres v1.0.8
 	gorm.io/gorm v1.20.12
 )
