ae9b33575ee7562093c126cc0b8ce8f34a2a2436
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-03-09 16:21:49 +0700

    feat: add encryption interface

---
+++ domain/encryption.go
@@ -0,0 +1,17 @@
+package domain
+
+// Encryptor does encrypt a plain text into an encrypted text
+type Encryptor interface {
+	Encrypt(string) (string, error)
+}
+
+// Decryptor does decrypt an encrypted text into a plain text
+type Decryptor interface {
+	Decrypt(string) (string, error)
+}
+
+// Crypto is for encrypting a plain text into an encrypted string and vice versa
+type Crypto interface {
+	Encryptor
+	Decryptor
+}
