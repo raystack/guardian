2e6079e24c8a4de92778b86769deeb0ead6ec3cb
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-03-15 11:55:07 +0700

    fix(resource): remove bulk upsert http handler

--- resource/handler.go
+++ resource/handler.go
@@ -1,12 +1,8 @@
 package resource
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/odpf/guardian/domain"
-	"github.com/odpf/guardian/utils"
 )
 
 // Handler for http service
@@ -15,23 +11,4 @@ type Handler struct {
 }
 
 // SetupHandler registers api handlers to the endpoints
-func SetupHandler(r *mux.Router, rs domain.ResourceService) {
-	h := &Handler{rs}
-	r.Methods(http.MethodPut).Path("/resources").HandlerFunc(h.BulkUpsert)
-}
-
-func (h *Handler) BulkUpsert(w http.ResponseWriter, r *http.Request) {
-	var payload []*domain.Resource
-	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if err := h.resourceService.BulkUpsert(payload); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	utils.ReturnJSON(w, payload)
-	return
-}
+func SetupHandler(r *mux.Router, rs domain.ResourceService) {}
