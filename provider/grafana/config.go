abaa0ce69243594d583283c91f092332646d77f3
Ishan Arya <ishanarya0@gmail.com>
2021-07-07 16:02:13 +0530

    feat(grafana): add provider configuration for grafana

---
+++ provider/grafana/config.go
@@ -0,0 +1,174 @@
+package grafana
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/mitchellh/mapstructure"
+	"github.com/odpf/guardian/domain"
+)
+
+type Credentials struct {
+	Host   string `json:"host" mapstructure:"host" validate:"required,url"`
+	ApiKey string `json:"api_key" mapstructure:"api_key" validate:"required"`
+}
+
+func (c *Credentials) Encrypt(encryptor domain.Encryptor) error {
+	if c == nil {
+		return ErrUnableToEncryptNilCredentials
+	}
+
+	encryptedApiKey, err := encryptor.Encrypt(c.ApiKey)
+	if err != nil {
+		return err
+	}
+
+	c.ApiKey = encryptedApiKey
+	return nil
+}
+
+func (c *Credentials) Decrypt(decryptor domain.Decryptor) error {
+	if c == nil {
+		return ErrUnableToDecryptNilCredentials
+	}
+
+	decryptedApiKey, err := decryptor.Decrypt(c.ApiKey)
+	if err != nil {
+		return err
+	}
+
+	c.ApiKey = decryptedApiKey
+	return nil
+}
+
+type PermissionConfig struct {
+	Name string `json:"name" mapstructure:"name" validate:"required,oneof=view edit admin"`
+}
+
+type Config struct {
+	ProviderConfig *domain.ProviderConfig
+	valid          bool
+
+	crypto    domain.Crypto
+	validator *validator.Validate
+}
+
+func NewConfig(pc *domain.ProviderConfig, crypto domain.Crypto) *Config {
+	return &Config{
+		ProviderConfig: pc,
+		validator:      validator.New(),
+		crypto:         crypto,
+	}
+}
+
+func (c *Config) ParseAndValidate() error {
+	return c.parseAndValidate()
+}
+
+func (c *Config) EncryptCredentials() error {
+	if err := c.parseAndValidate(); err != nil {
+		return err
+	}
+
+	credentials, ok := c.ProviderConfig.Credentials.(*Credentials)
+	if !ok {
+		return ErrInvalidCredentials
+	}
+
+	if err := credentials.Encrypt(c.crypto); err != nil {
+		return err
+	}
+
+	c.ProviderConfig.Credentials = credentials
+	return nil
+}
+
+func (c *Config) parseAndValidate() error {
+	if c.valid {
+		return nil
+	}
+
+	validationErrors := []error{}
+
+	if credentials, err := c.validateCredentials(c.ProviderConfig.Credentials); err != nil {
+		validationErrors = append(validationErrors, err)
+	} else {
+		c.ProviderConfig.Credentials = credentials
+	}
+
+	for _, r := range c.ProviderConfig.Resources {
+		if err := c.validateResourceConfig(r); err != nil {
+			validationErrors = append(validationErrors, err)
+		}
+	}
+
+	if len(validationErrors) > 0 {
+		errorStrings := []string{}
+		for _, err := range validationErrors {
+			errorStrings = append(errorStrings, err.Error())
+		}
+		return errors.New(strings.Join(errorStrings, "\n"))
+	}
+
+	c.valid = true
+	return nil
+}
+
+func (c *Config) validateCredentials(value interface{}) (*Credentials, error) {
+	var credentials Credentials
+	if err := mapstructure.Decode(value, &credentials); err != nil {
+		return nil, err
+	}
+
+	if err := c.validator.Struct(credentials); err != nil {
+		return nil, err
+	}
+
+	return &credentials, nil
+}
+
+func (c *Config) validateResourceConfig(resource *domain.ResourceConfig) error {
+	resourceTypeValidation := fmt.Sprintf("oneof=%s %s", ResourceTypeFolder, ResourceTypeDashboard)
+	if err := c.validator.Var(resource.Type, resourceTypeValidation); err != nil {
+		return err
+	}
+
+	for _, role := range resource.Roles {
+		for i, permission := range role.Permissions {
+			if permissionConfig, err := c.validatePermission(resource.Type, permission); err != nil {
+				return err
+			} else {
+				role.Permissions[i] = permissionConfig
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *Config) validatePermission(resourceType string, value interface{}) (*PermissionConfig, error) {
+	permissionConfig, ok := value.(map[string]interface{})
+	if !ok {
+		return nil, ErrInvalidPermissionConfig
+	}
+
+	var pc PermissionConfig
+	if err := mapstructure.Decode(permissionConfig, &pc); err != nil {
+		return nil, err
+	}
+
+	var nameValidation string
+	if resourceType == ResourceTypeFolder {
+		nameValidation = "oneof=view edit admin"
+	} else if resourceType == ResourceTypeDashboard {
+		nameValidation = "oneof=view edit admin"
+	}
+
+	if err := c.validator.Var(pc.Name, nameValidation); err != nil {
+		return nil, err
+	}
+
+	return &pc, nil
+}
