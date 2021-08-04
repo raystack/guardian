710c8aaea8663326bff3c44a2655af0f0cab9ce5
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-08-04 23:23:05 +0700

    fix: change update approval http request body

--- api/proto/guardian/guardian.pb.gw.go
+++ api/proto/guardian/guardian.pb.gw.go
@@ -627,7 +627,7 @@ func request_GuardianService_UpdateApproval_0(ctx context.Context, marshaler run
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Action); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -671,7 +671,7 @@ func local_request_GuardianService_UpdateApproval_0(ctx context.Context, marshal
 	if berr != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq.Action); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
