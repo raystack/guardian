9bfb4ee7cf49a6b12dfdcb0d017f2222c3d68199
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-08-13 09:35:38 +0700

    feat: update resource metadata

--- cmd/resources.go
+++ cmd/resources.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	pb "github.com/odpf/guardian/api/proto/odpf/guardian"
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func resourcesCommand(c *config) *cobra.Command {
@@ -17,6 +19,7 @@ func resourcesCommand(c *config) *cobra.Command {
 	}
 
 	cmd.AddCommand(listResourcesCommand(c))
+	cmd.AddCommand(metadataCommand(c))
 
 	return cmd
 }
@@ -56,4 +59,69 @@ func listResourcesCommand(c *config) *cobra.Command {
 			return nil
 		},
 	}
+}
+
+func metadataCommand(c *config) *cobra.Command {
+	var id uint
+	var values []string
+
+	cmd := &cobra.Command{
+		Use:   "metadata",
+		Short: "manage resource's metadata",
+	}
+
+	setCmd := &cobra.Command{
+		Use:   "set",
+		Short: "store new metadata",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			metadata := map[string]interface{}{}
+			for _, a := range values {
+				items := strings.Split(a, "=")
+				key := items[0]
+				value := items[1]
+
+				metadata[key] = value
+			}
+			metadataProto, err := structpb.NewStruct(metadata)
+			if err != nil {
+				return err
+			}
+
+			dialTimeoutCtx, dialCancel := context.WithTimeout(context.Background(), time.Second*2)
+			defer dialCancel()
+			conn, err := createConnection(dialTimeoutCtx, c.Host)
+			if err != nil {
+				return err
+			}
+			defer conn.Close()
+			client := pb.NewGuardianServiceClient(conn)
+
+			// TODO: get one resource
+
+			requestTimeoutCtx, requestTimeoutCtxCancel := context.WithTimeout(context.Background(), time.Second*2)
+			defer requestTimeoutCtxCancel()
+			_, err = client.UpdateResource(requestTimeoutCtx, &pb.UpdateResourceRequest{
+				Id: uint32(id),
+				Resource: &pb.Resource{
+					Details: metadataProto,
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("metadata updated")
+
+			return nil
+		},
+	}
+
+	setCmd.Flags().StringArrayVar(&values, "values", []string{}, "list of key-value pair. Example: key=value foo=bar")
+
+	cmd.AddCommand(setCmd)
+	cmd.PersistentFlags().UintVar(&id, "id", 0, "resource id")
+	cmd.MarkPersistentFlagRequired("id")
+
+	return cmd
 }
