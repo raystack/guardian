f956bc72d9d5256352c4ed08a65c1b1868dccea7
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-09-16 10:23:47 +0700

    refactor: add validations in policy create & update

--- policy/errors.go
+++ policy/errors.go
@@ -6,5 +6,9 @@ var (
 	// ErrEmptyIDParam is the error value if the policy id is empty
 	ErrEmptyIDParam = errors.New("id can't be empty")
 	// ErrPolicyDoesNotExists is the error value if the designated policy is not exists
-	ErrPolicyDoesNotExists = errors.New("policy does not exists")
+	ErrPolicyDoesNotExists         = errors.New("policy does not exists")
+	ErrIDContainsWhitespaces       = errors.New("id should not contain whitespaces")
+	ErrStepNameContainsWhitespaces = errors.New("step name should not contain whitespaces")
+	ErrInvalidApprovers            = errors.New("invalid approvers")
+	ErrStepDependencyDoesNotExists = errors.New("step dependency does not exists")
 )
