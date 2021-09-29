36160e0cca3dc23c0b22ce0f6990d8a56f44f8ff
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2021-09-29 10:01:23 +0700

    feat: add provider get command

--- cmd/providers.go
+++ cmd/providers.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/MakeNowJust/heredoc"
 	v1 "github.com/odpf/guardian/api/handler/v1"
 	pb "github.com/odpf/guardian/api/proto/odpf/guardian"
 	"github.com/odpf/guardian/app"
@@ -19,6 +21,7 @@ func providersCommand(c *app.CLIConfig, adapter v1.ProtoAdapter) *cobra.Command 
 	}
 
 	cmd.AddCommand(listProvidersCommand(c))
+	cmd.AddCommand(getProviderCmd(c, adapter))
 	cmd.AddCommand(createProviderCommand(c, adapter))
 	cmd.AddCommand(updateProviderCommand(c, adapter))
 
@@ -54,6 +57,58 @@ func listProvidersCommand(c *app.CLIConfig) *cobra.Command {
 			return nil
 		},
 	}
+}
+
+func getProviderCmd(c *app.CLIConfig, adapter v1.ProtoAdapter) *cobra.Command {
+	var format string
+	cmd := &cobra.Command{
+		Use:   "get",
+		Short: "Get a provider details",
+		Example: heredoc.Doc(`
+			$ guardian provider get 1
+		`),
+		Annotations: map[string]string{
+			"group:core": "true",
+		},
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			client, cancel, err := createClient(ctx, c.Host)
+			if err != nil {
+				return err
+			}
+			defer cancel()
+
+			id, err := strconv.ParseUint(args[0], 10, 32)
+			if err != nil {
+				return fmt.Errorf("invalid provider id: %v", err)
+			}
+
+			res, err := client.GetProvider(ctx, &pb.GetProviderRequest{
+				Id: uint32(id),
+			})
+			if err != nil {
+				return err
+			}
+
+			p, err := adapter.FromProviderProto(res)
+			if err != nil {
+				return fmt.Errorf("failed to parse provider: %v", err)
+			}
+
+			formattedResult, err := outputFormat(p, format)
+			if err != nil {
+				return fmt.Errorf("failed to format provider: %v", err)
+			}
+
+			fmt.Print(string(formattedResult))
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&format, "format", "yaml", "Print output with the selected format")
+
+	return cmd
 }
 
 func createProviderCommand(c *app.CLIConfig, adapter v1.ProtoAdapter) *cobra.Command {
