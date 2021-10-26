cf764eb4bf19ae9f42eda43a77f782b0a4ccf2c4
rahmatrhd <rahmatramahidayat@gmail.com>
2021-10-26 10:53:11 +0700

    refactor(iam): user enum for provider type constants

--- iam/client.go
+++ iam/client.go
@@ -6,13 +6,15 @@ import (
 	"github.com/odpf/guardian/domain"
 )
 
+type ProviderType string
+
 const (
-	IAMProviderShield = "shield"
-	IAMProviderHTTP   = "http"
+	ProviderTypeShield ProviderType = "shield"
+	ProviderTypeHTTP   ProviderType = "http"
 )
 
 type ClientConfig struct {
-	Provider string `mapstructure:"provider"`
+	Provider ProviderType `mapstructure:"provider"`
 
 	// shield config
 	Host string `mapstructure:"host" validate:"required_if=Provider shield"`
@@ -24,11 +26,11 @@ type ClientConfig struct {
 }
 
 func NewClient(config *ClientConfig) (domain.IAMClient, error) {
-	if config.Provider == IAMProviderShield {
+	if config.Provider == ProviderTypeShield {
 		return NewShieldClient(&ShieldClientConfig{
 			Host: config.Host,
 		})
-	} else if config.Provider == IAMProviderHTTP {
+	} else if config.Provider == ProviderTypeHTTP {
 		return NewHTTPClient(&HTTPClientConfig{
 			URL:     config.URL,
 			Auth:    config.Auth,
