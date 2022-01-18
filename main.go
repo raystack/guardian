9cb95bf00b422b0061395149b5b89503c23301c9
Ishan Arya <ishanarya0@gmail.com>
2022-01-18 23:35:01 +0530

    feat: to print better errors

--- main.go
+++ main.go
@@ -28,7 +28,8 @@ func main() {
 	root := cmd.New(cliConfig)
 
 	if cmd, err := root.ExecuteC(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		//fmt.Fprintln(os.Stderr, err)
+		printError(err)
 
 		cmdErr := strings.HasPrefix(err.Error(), "unknown command")
 		flagErr := strings.HasPrefix(err.Error(), "unknown flag")
@@ -46,4 +47,24 @@ func main() {
 	}
 
 	// TODO: Notify if updated version is available
+}
+
+func printError(err error) {
+	e := err.Error()
+	if strings.Split(e, ":")[0] == "rpc error" {
+		es := strings.Split(e, "= ")
+
+		em := es[2]
+		errMsg := "Error: " + em
+
+		ec := es[1]
+		errCode := ec[0 : len(ec)-5]
+		errCode = "Code: " + errCode
+
+		fmt.Fprintln(os.Stderr, errCode, errMsg)
+	} else {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	return
 }
