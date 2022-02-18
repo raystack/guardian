d4068a0a7903aca318a3163c3e62c7c041321864
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2022-02-18 09:13:02 +0700

    chore(appeal): return list of user's appeal filtered by created_by

--- domain/appeal.go
+++ domain/appeal.go
@@ -97,6 +97,7 @@ type ApprovalAction struct {
 }
 
 type ListAppealsFilter struct {
+	CreatedBy                 string    `mapstructure:"created_by" validate:"omitempty,required"`
 	AccountID                 string    `mapstructure:"account_id" validate:"omitempty,required"`
 	ResourceID                string    `mapstructure:"resource_id" validate:"omitempty,required"`
 	Role                      string    `mapstructure:"role" validate:"omitempty,required"`
