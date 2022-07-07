50e0502e06475899f2f7788b634a36e15d2a4417
Rishab Jain <f20190124@hyderabad.bits-pilani.ac.in>
2022-07-07 10:28:23 +0530

    chore: add tests for grafana provider and resource

--- plugins/providers/grafana/provider_test.go
+++ plugins/providers/grafana/provider_test.go
@@ -429,3 +429,356 @@ func TestGrantAccess(t *testing.T) {
 		})
 	})
 }
+
+func TestRevokeAccess(t *testing.T) {
+	t.Run("should return an error if there is an error in getting permissions", func(t *testing.T) {
+		var permission grafana.Permission
+		invalidPermissionConfig := map[string]interface{}{}
+		invalidPermissionConfigError := mapstructure.Decode(invalidPermissionConfig, &permission)
+
+		testcases := []struct {
+			resourceConfigs []*domain.ResourceConfig
+			appeal          *domain.Appeal
+			expectedError   error
+		}{
+			{
+				appeal: &domain.Appeal{
+					Resource: &domain.Resource{
+						Type: "test-type",
+					},
+				},
+				expectedError: grafana.ErrInvalidResourceType,
+			},
+			{
+				resourceConfigs: []*domain.ResourceConfig{
+					{
+						Type: "test-type",
+						Roles: []*domain.Role{
+							{
+								ID: "not-test-role",
+							},
+						},
+					},
+				},
+				appeal: &domain.Appeal{
+					Resource: &domain.Resource{
+						Type: "test-type",
+					},
+					Role: "test-role",
+				},
+				expectedError: grafana.ErrInvalidRole,
+			},
+			{
+				resourceConfigs: []*domain.ResourceConfig{
+					{
+						Type: "test-type",
+						Roles: []*domain.Role{
+							{
+								ID: "test-role",
+								Permissions: []interface{}{
+									invalidPermissionConfig,
+								},
+							},
+						},
+					},
+				},
+				appeal: &domain.Appeal{
+					Resource: &domain.Resource{
+						Type: "test-type",
+					},
+					Role: "test-role",
+				},
+				expectedError: invalidPermissionConfigError,
+			},
+		}
+
+		for _, tc := range testcases {
+			crypto := new(mocks.Crypto)
+			p := grafana.NewProvider("", crypto)
+
+			providerConfig := &domain.ProviderConfig{
+				Resources: tc.resourceConfigs,
+			}
+
+			actualError := p.RevokeAccess(providerConfig, tc.appeal)
+			assert.EqualError(t, actualError, tc.expectedError.Error())
+		}
+	})
+
+	t.Run("should return error if credentials is invalid", func(t *testing.T) {
+		crypto := new(mocks.Crypto)
+		p := grafana.NewProvider("", crypto)
+
+		pc := &domain.ProviderConfig{
+			Credentials: "invalid-credentials",
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: "test-type",
+					Roles: []*domain.Role{
+						{
+							ID:          "test-role",
+							Permissions: []interface{}{"test-permission-config"},
+						},
+					},
+				},
+			},
+		}
+		a := &domain.Appeal{
+			Resource: &domain.Resource{
+				Type: "test-type",
+			},
+			Role: "test-role",
+		}
+
+		actualError := p.RevokeAccess(pc, a)
+		assert.Error(t, actualError)
+	})
+
+	t.Run("should return error if there are any on client initialization", func(t *testing.T) {
+		password := "test-password"
+		crypto := new(mocks.Crypto)
+		p := grafana.NewProvider("", crypto)
+		expectedError := errors.New("decrypt error")
+		crypto.On("Decrypt", password).Return("", expectedError).Once()
+
+		pc := &domain.ProviderConfig{
+			Credentials: grafana.Credentials{
+				Host:     "localhost",
+				Username: "test-username",
+				Password: password,
+			},
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: "test-type",
+					Roles: []*domain.Role{
+						{
+							ID:          "test-role",
+							Permissions: []interface{}{"test-permission-config"},
+						},
+					},
+				},
+			},
+		}
+		a := &domain.Appeal{
+			Resource: &domain.Resource{
+				Type: "test-type",
+			},
+			Role: "test-role",
+		}
+
+		actualError := p.RevokeAccess(pc, a)
+
+		assert.EqualError(t, actualError, expectedError.Error())
+	})
+
+	t.Run("should return error if resource type in unknown", func(t *testing.T) {
+		password := "test-password"
+		crypto := new(mocks.Crypto)
+		p := grafana.NewProvider("", crypto)
+		expectedError := errors.New("invalid resource type")
+		crypto.On("Decrypt", password).Return("", expectedError).Once()
+
+		pc := &domain.ProviderConfig{
+			Credentials: grafana.Credentials{
+				Host:     "localhost",
+				Username: "test-username",
+				Password: password,
+			},
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: "test-type",
+					Roles: []*domain.Role{
+						{
+							ID:          "test-role",
+							Permissions: []interface{}{"test-permission-config"},
+						},
+					},
+				},
+			},
+			URN: "test-urn",
+		}
+		a := &domain.Appeal{
+			Resource: &domain.Resource{
+				Type: "test-type",
+			},
+			Role: "test-role",
+		}
+
+		actualError := p.RevokeAccess(pc, a)
+
+		assert.EqualError(t, actualError, expectedError.Error())
+	})
+
+	t.Run("given dashboard resource", func(t *testing.T) {
+		t.Run("should return error if there is an error in granting dashboard access", func(t *testing.T) {
+			providerURN := "test-provider-urn"
+			expectedError := errors.New("client error")
+			crypto := new(mocks.Crypto)
+			client := new(mocks.GrafanaClient)
+			p := grafana.NewProvider("", crypto)
+			p.Clients = map[string]grafana.GrafanaClient{
+				providerURN: client,
+			}
+			client.On("RevokeDashboardAccess", mock.Anything, mock.Anything, mock.Anything).Return(expectedError).Once()
+
+			pc := &domain.ProviderConfig{
+				Credentials: grafana.Credentials{
+					Host:     "localhost",
+					Username: "test-username",
+					Password: "test-password",
+				},
+				Resources: []*domain.ResourceConfig{
+					{
+						Type: grafana.ResourceTypeDashboard,
+						Roles: []*domain.Role{
+							{
+								ID:          "test-role",
+								Permissions: []interface{}{"test-permission-config"},
+							},
+						},
+					},
+				},
+				URN: providerURN,
+			}
+			a := &domain.Appeal{
+				Resource: &domain.Resource{
+					Type: grafana.ResourceTypeDashboard,
+					URN:  "999",
+					Name: "test-dashboard",
+				},
+				Role: "test-role",
+			}
+
+			actualError := p.RevokeAccess(pc, a)
+
+			assert.EqualError(t, actualError, expectedError.Error())
+		})
+
+	})
+
+	t.Run("should return nil error if granting access is successful", func(t *testing.T) {
+		providerURN := "test-provider-urn"
+		crypto := new(mocks.Crypto)
+		client := new(mocks.GrafanaClient)
+		expectedDatabase := &grafana.Dashboard{
+			Title: "test-dashboard",
+			ID:    999,
+		}
+		expectedUser := "test@email.com"
+		expectedRole := grafana.DashboardRoleViewer
+		p := grafana.NewProvider("", crypto)
+		p.Clients = map[string]grafana.GrafanaClient{
+			providerURN: client,
+		}
+		client.On("RevokeDashboardAccess", expectedDatabase, expectedUser, expectedRole).Return(nil).Once()
+
+		pc := &domain.ProviderConfig{
+			Credentials: grafana.Credentials{
+				Host:     "localhost",
+				Username: "test-username",
+				Password: "test-password",
+			},
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: grafana.ResourceTypeDashboard,
+					Roles: []*domain.Role{
+						{
+							ID:          "viewer",
+							Permissions: []interface{}{"view"},
+						},
+					},
+				},
+			},
+			URN: providerURN,
+		}
+		a := &domain.Appeal{
+			Resource: &domain.Resource{
+				Type: grafana.ResourceTypeDashboard,
+				URN:  "999",
+				Name: "test-dashboard",
+			},
+			Role:       "viewer",
+			AccountID:  expectedUser,
+			ResourceID: "999",
+			ID:         "999",
+		}
+
+		actualError := p.RevokeAccess(pc, a)
+
+		assert.Nil(t, actualError)
+	})
+
+}
+
+func TestGetRoles(t *testing.T) {
+
+	t.Run("should return an error if invalid resource type", func(t *testing.T) {
+		expectedError := grafana.ErrInvalidResourceType
+		crypto := new(mocks.Crypto)
+		pv := grafana.NewProvider("grafana", crypto)
+		validconfig := &domain.ProviderConfig{
+			Type: "grafana",
+			Credentials: grafana.Credentials{
+				Host:     "localhost",
+				Username: "test-username",
+				Password: "test-password",
+			},
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: grafana.ResourceTypeDashboard,
+				},
+			},
+			URN: "test-URN",
+		}
+
+		actualRoles, actualError := pv.GetRoles(validconfig, "wrong-resource-type")
+
+		assert.Nil(t, actualRoles)
+		assert.Equal(t, expectedError, actualError)
+	},
+	)
+
+	t.Run("should return no error if returned roles match the expected roles", func(t *testing.T) {
+		expectedRoles := []*domain.Role{
+			{
+				ID:          "viewer",
+				Permissions: []interface{}{"view"},
+			},
+		}
+		crypto := new(mocks.Crypto)
+		pv := grafana.NewProvider("grafana", crypto)
+		validconfig := &domain.ProviderConfig{
+			Type: "grafana",
+			Credentials: grafana.Credentials{
+				Host:     "localhost",
+				Username: "test-username",
+				Password: "test-password",
+			},
+			Resources: []*domain.ResourceConfig{
+				{
+					Type:  grafana.ResourceTypeDashboard,
+					Roles: expectedRoles,
+				},
+			},
+			URN: "test-URN",
+		}
+
+		actualRoles, actualError := pv.GetRoles(validconfig, "dashboard")
+
+		assert.Equal(t, expectedRoles, actualRoles)
+		assert.NoError(t, actualError)
+	},
+	)
+}
+
+func TestGetAccountTypes(t *testing.T) {
+	t.Run("should return the list of supported account types (user only)", func(t *testing.T) {
+		crypto := new(mocks.Crypto)
+		pv := grafana.NewProvider("grafana", crypto)
+		expectedAccountTypes := []string{"user"}
+
+		actualAccountTypes := pv.GetAccountTypes()
+
+		assert.Equal(t, expectedAccountTypes, actualAccountTypes)
+	})
+}
