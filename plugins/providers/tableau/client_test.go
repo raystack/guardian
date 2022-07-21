9cf10c9912ba1a572ec406a12a2ec70e87b4ccac
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2022-07-21 18:51:47 +0700

    test(tableau): fix test for tableau client

--- plugins/providers/tableau/client_test.go
+++ plugins/providers/tableau/client_test.go
@@ -2,7 +2,10 @@ package tableau_test
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -89,6 +92,7 @@ type ClientTestSuite struct {
 	host           string
 	apiVersion     string
 	siteID         string
+	userID         string
 }
 
 func TestClient(t *testing.T) {
@@ -99,15 +103,16 @@ func (s *ClientTestSuite) setup() {
 	s.mockHttpClient = new(mocks.HTTPClient)
 
 	s.sessionToken = "93df71b4-6887-46bd-b4bf-7ad3b94bd6fe"
+	s.siteID = "1.0"
+	s.userID = "test-user-id"
+	sessionResponseJSON := fmt.Sprintf(`{"credentials":{"token":"%s","site":{"id":"%s"},"user":{"id":"%s"}}}`, s.sessionToken, s.siteID, s.userID)
 	sessionResponse := http.Response{
 		StatusCode: 200,
-		Body:       ioutil.NopCloser(bytes.NewReader([]byte(`{"id":"` + s.sessionToken + `"}`))),
+		Body:       ioutil.NopCloser(bytes.NewReader([]byte(sessionResponseJSON))),
 	}
 	s.mockHttpClient.On("Do", mock.Anything).Return(&sessionResponse, nil).Once()
 	s.apiVersion = "3.12"
-	s.siteID = "1.0"
 	s.host = "http://localhost"
-	//siteID := "test-site"
 	client, err := tableau.NewClient(&tableau.ClientConfig{
 		Username:   "test-username",
 		Password:   "test-password",
@@ -121,55 +126,52 @@ func (s *ClientTestSuite) setup() {
 	s.client = client
 }
 
-// func (s *ClientTestSuite) TestGetWorkbooks() {
-// 	s.Run("should get workbooks and nil error on success", func() {
-// 		s.setup()
+func (s *ClientTestSuite) TestGetWorkbooks() {
+	s.Run("should get workbooks and nil error on success", func() {
+		s.setup()
 
-// 		url := fmt.Sprintf("/api/%v/sites/%v/workbooks", s.apiVersion, s.siteID)
+		url := fmt.Sprintf("/api/%v/sites/%v/workbooks", s.apiVersion, s.siteID)
+		testRequest, err := s.getTestRequest(http.MethodGet, url, nil)
+		s.Require().NoError(err)
 
-// 		testRequest, err := s.getTestRequest(http.MethodGet, url, nil)
-// 		s.Require().NoError(err)
+		workbookResponseJSON := `{"pagination":{"pageNumber":"1","pageSize":"1","totalAvailable":"1"},"workbooks":{"workbook":[{"id":"test-workbook"}]}}`
+		workbookResponse := http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader([]byte(workbookResponseJSON)))}
+		s.mockHttpClient.On("Do", testRequest).Return(&workbookResponse, nil).Once()
 
-// 		workbookResponseJSON := `[{}]`
-// 		workbookResponse := http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader([]byte(workbookResponseJSON)))}
-// 		s.mockHttpClient.On("Do", testRequest).Return(&workbookResponse, nil).Once()
+		expectedWorkbooks := []*tableau.Workbook{
+			{
+				ID: "test-workbook",
+			},
+		}
 
-// 		expectedWorkbooks := []tableau.Workbook{
-// 			{},
-// 		}
+		actualWorkbooks, err1 := s.client.GetWorkbooks()
 
-// 		result, err1 := s.client.GetWorkbooks()
-// 		var workbooks []tableau.Workbook
-// 		for _, wb := range result {
-// 			workbooks = append(workbooks, *wb)
-// 		}
-// 		s.Nil(err1)
-// 		s.ElementsMatch(expectedWorkbooks, workbooks)
+		s.Nil(err1)
+		s.Equal(expectedWorkbooks, actualWorkbooks)
+	})
+}
 
-// 	})
-// }
-
-// func (s *ClientTestSuite) getTestRequest(method, path string, body interface{}) (*http.Request, error) {
-// 	var buf io.ReadWriter
-// 	if body != nil {
-// 		buf = new(bytes.Buffer)
-// 		err := json.NewEncoder(buf).Encode(body)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 	}
-// 	url := fmt.Sprintf("%s%s", s.host, path)
-// 	req, err := http.NewRequest(method, url, buf)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if body != nil {
-// 		req.Header.Set("Content-Type", "application/json")
-// 	}
-// 	req.Header.Set("Accept", "application/json")
-// 	req.Header.Set("X-Tableau-Auth", s.sessionToken)
-// 	return req, nil
-// }
+func (s *ClientTestSuite) getTestRequest(method, path string, body interface{}) (*http.Request, error) {
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+	url := fmt.Sprintf("%s%s", s.host, path)
+	req, err := http.NewRequest(method, url, buf)
+	if err != nil {
+		return nil, err
+	}
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("X-Tableau-Auth", s.sessionToken)
+	return req, nil
+}
 
 // func (s *ClientTestSuite) TestGetFlows() {
 // 	s.Run("should get folders and nil error on success", func() {
