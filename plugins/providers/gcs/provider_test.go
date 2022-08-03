a49cc6da68906d6c3867b9ddeeb53c7864754cfa
Rishab Jain <f20190124@hyderabad.bits-pilani.ac.in>
2022-08-03 16:55:12 +0530

    test: update test coverage for revoke access and create config funcions

--- plugins/providers/gcs/provider_test.go
+++ plugins/providers/gcs/provider_test.go
@@ -27,6 +27,97 @@ func TestGetType(t *testing.T) {
 }
 
 func TestCreateConfig(t *testing.T) {
+	t.Run("should return error if error in parse and validate configurations", func(t *testing.T) {
+		crypto := new(mocks.Crypto)
+		client := new(mocks.GcsClient)
+		p := gcs.NewProvider("", crypto)
+		p.Clients = map[string]gcs.GcsClient{
+			"test-resource-name": client,
+		}
+
+		testcases := []struct {
+			name string
+			pc   *domain.ProviderConfig
+		}{
+			{
+				name: "invalid resource type",
+				pc: &domain.ProviderConfig{
+					Credentials: gcs.Credentials{
+						ServiceAccountKey: base64.StdEncoding.EncodeToString([]byte(`{"type":"service_account"}`)),
+						ResourceName:      "projects/test-resource-name",
+					},
+					Resources: []*domain.ResourceConfig{
+						{
+							Type: "not dataset or table resource type",
+							Roles: []*domain.Role{
+								{
+									ID:          "viewer",
+									Permissions: []interface{}{"wrong permissions"},
+								},
+							},
+						},
+					},
+				},
+			},
+			{
+				name: "invalid permissions for bucket resource type",
+				pc: &domain.ProviderConfig{
+					Credentials: gcs.Credentials{
+						ServiceAccountKey: base64.StdEncoding.EncodeToString([]byte(`{"type":"service_account"}`)),
+						ResourceName:      "projects/test-resource-name",
+					},
+					Resources: []*domain.ResourceConfig{
+						{
+							Type: gcs.ResourceTypeBucket,
+							Roles: []*domain.Role{
+								{
+									ID:          "viewer",
+									Permissions: []interface{}{"wrong permissions"},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		crypto.On("Encrypt", `{"type":"service_account"}`).Return(`{"type":"service_account"}`, nil)
+
+		for _, tc := range testcases {
+			t.Run(tc.name, func(t *testing.T) {
+				actualError := p.CreateConfig(tc.pc)
+				assert.Error(t, actualError)
+			})
+		}
+	})
+
+	t.Run("should return error if error in encrypting the credentials", func(t *testing.T) {
+		providerURN := "test-URN"
+		crypto := new(mocks.Crypto)
+		client := new(mocks.GcsClient)
+		p := gcs.NewProvider("", crypto)
+		p.Clients = map[string]gcs.GcsClient{
+			"test-resource-name": client,
+		}
+		pc := &domain.ProviderConfig{
+			Resources: []*domain.ResourceConfig{
+				{
+					Type:  gcs.ResourceTypeBucket,
+					Roles: []*domain.Role{},
+				},
+			},
+			Credentials: gcs.Credentials{
+				ServiceAccountKey: base64.StdEncoding.EncodeToString([]byte(`{"type":"service_account"}`)),
+				ResourceName:      "projects/test-resource-name",
+			},
+			URN: providerURN,
+		}
+		expectedError := errors.New("error in encrypting SAK")
+		crypto.On("Encrypt", `{"type":"service_account"}`).Return("", expectedError)
+		actualError := p.CreateConfig(pc)
+
+		assert.Equal(t, expectedError, actualError)
+	})
+
 	t.Run("should make the provider config, parse and validate the credentials and permissions and return nil error on success", func(t *testing.T) {
 		crypto := new(mocks.Crypto)
 		p := gcs.NewProvider("gcs", crypto)
@@ -472,6 +563,253 @@ func TestGrantAccess(t *testing.T) {
 }
 
 func TestRevokeAccess(t *testing.T) {
+
+	t.Run("should return error if Provider Config or Appeal doesn't have required parameters", func(t *testing.T) {
+		testCases := []struct {
+			name           string
+			providerConfig *domain.ProviderConfig
+			appeal         *domain.Appeal
+			expectedError  error
+		}{
+			{
+				name:           "nil provider config",
+				providerConfig: nil,
+				expectedError:  fmt.Errorf("invalid provider/appeal config: %w", gcs.ErrNilProviderConfig),
+			},
+			{
+				name: "nil appeal config",
+				providerConfig: &domain.ProviderConfig{
+					Type:                domain.ProviderTypeGCS,
+					URN:                 "test-URN",
+					AllowedAccountTypes: []string{"user", "serviceAccount"},
+				},
+				appeal:        nil,
+				expectedError: fmt.Errorf("invalid provider/appeal config: %w", gcs.ErrNilAppeal),
+			},
+			{
+				name: "nil resource config",
+				providerConfig: &domain.ProviderConfig{
+					Type:                domain.ProviderTypeGCS,
+					URN:                 "test-URN",
+					AllowedAccountTypes: []string{"user", "serviceAccount"},
+				},
+				appeal: &domain.Appeal{
+					ID:          "test-appeal-id",
+					AccountType: "user",
+				},
+				expectedError: fmt.Errorf("invalid provider/appeal config: %w", gcs.ErrNilResource),
+			},
+			{
+				name: "provider type doesnt match",
+				providerConfig: &domain.ProviderConfig{
+					Type:                domain.ProviderTypeGCS,
+					URN:                 "test-URN-1",
+					AllowedAccountTypes: []string{"user", "serviceAccount"},
+				},
+				appeal: &domain.Appeal{
+					ID:          "test-appeal-id",
+					AccountType: "user",
+					Resource: &domain.Resource{
+						ID:           "test-resource-id",
+						ProviderType: "not-gcs",
+					},
+				},
+				expectedError: fmt.Errorf("invalid provider/appeal config: %w", gcs.ErrProviderTypeMismatch),
+			},
+			{
+				name: "provider urn doesnt match",
+				providerConfig: &domain.ProviderConfig{
+					Type:                domain.ProviderTypeGCS,
+					URN:                 "test-URN-1",
+					AllowedAccountTypes: []string{"user", "serviceAccount"},
+				},
+				appeal: &domain.Appeal{
+					ID:          "test-appeal-id",
+					AccountType: "user",
+					Resource: &domain.Resource{
+						ID:           "test-resource-id",
+						ProviderType: domain.ProviderTypeGCS,
+						ProviderURN:  "not-test-URN-1",
+					},
+				},
+				expectedError: fmt.Errorf("invalid provider/appeal config: %w", gcs.ErrProviderURNMismatch),
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				p := initProvider()
+				pc := tc.providerConfig
+				a := tc.appeal
+
+				actualError := p.RevokeAccess(pc, a)
+				assert.EqualError(t, actualError, tc.expectedError.Error())
+			})
+		}
+	})
+
+	t.Run("should return an error if there is an error in getting permissions", func(t *testing.T) {
+		var permission gcs.Permission
+		invalidPermissionConfig := map[string]interface{}{}
+		invalidPermissionConfigError := mapstructure.Decode(invalidPermissionConfig, &permission)
+
+		testCases := []struct {
+			name            string
+			resourceConfigs []*domain.ResourceConfig
+			appeal          *domain.Appeal
+			expectedError   error
+		}{
+			{
+				name: "invalid resource type",
+				appeal: &domain.Appeal{
+					Resource: &domain.Resource{
+						Type: "test-type",
+					},
+				},
+				expectedError: fmt.Errorf("error in getting permissions: %w", gcs.ErrInvalidResourceType),
+			},
+			{
+				name: "invalid role",
+				resourceConfigs: []*domain.ResourceConfig{
+					{
+						Type: "test-type",
+						Roles: []*domain.Role{
+							{
+								ID: "not-test-role",
+							},
+						},
+					},
+				},
+				appeal: &domain.Appeal{
+					Resource: &domain.Resource{
+						Type: "test-type",
+					},
+					Role: "test-role",
+				},
+				expectedError: fmt.Errorf("error in getting permissions: %w", gcs.ErrInvalidRole),
+			},
+			{
+				name: "invalid permissions config",
+				resourceConfigs: []*domain.ResourceConfig{
+					{
+						Type: "test-type",
+						Roles: []*domain.Role{
+							{
+								ID: "test-role",
+								Permissions: []interface{}{
+									invalidPermissionConfig,
+								},
+							},
+						},
+					},
+				},
+				appeal: &domain.Appeal{
+					Resource: &domain.Resource{
+						Type: "test-type",
+					},
+					Role: "test-role",
+				},
+				expectedError: fmt.Errorf("error in getting permissions: %w", invalidPermissionConfigError),
+			},
+		}
+
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				crypto := new(mocks.Crypto)
+				p := gcs.NewProvider("", crypto)
+
+				providerConfig := &domain.ProviderConfig{
+					Resources: tc.resourceConfigs,
+				}
+
+				actualError := p.RevokeAccess(providerConfig, tc.appeal)
+				assert.EqualError(t, actualError, tc.expectedError.Error())
+			})
+		}
+	},
+	)
+
+	t.Run("should return error if error in decoding credentials", func(t *testing.T) {
+		p := initProvider()
+
+		pc := &domain.ProviderConfig{
+			Credentials: "invalid-credentials-struct",
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: "test-type",
+					Roles: []*domain.Role{
+						{
+							ID:          "test-role",
+							Permissions: []interface{}{"test-permission-config"},
+						},
+					},
+				},
+			},
+		}
+		a := &domain.Appeal{
+			Resource: &domain.Resource{
+				Type: "test-type",
+			},
+			Role: "test-role",
+		}
+		actualError := p.RevokeAccess(pc, a)
+		assert.Error(t, actualError)
+	})
+
+	t.Run("should return error if error in decrypting the service account key", func(t *testing.T) {
+		expectedAccountType := "user"
+		expectedAccountID := "test@email.com"
+		crypto := new(mocks.Crypto)
+		client := new(mocks.GcsClient)
+		p := gcs.NewProvider("gcs", crypto)
+		p.Clients = map[string]gcs.GcsClient{
+			"test-resource-name": client,
+		}
+		providerURN := "test-resource-name"
+		expectedError := errors.New("Error in decrypting service account key")
+		crypto.On("Decrypt", "c2VydmljZV9hY2NvdW50LWtleS1qc29u").Return(`{"type":"service_account"}`, expectedError)
+
+		pc := &domain.ProviderConfig{
+			Type: domain.ProviderTypeGCS,
+			URN:  providerURN,
+			Credentials: gcs.Credentials{
+				ServiceAccountKey: base64.StdEncoding.EncodeToString([]byte("service_account-key-json")),
+				ResourceName:      "projects/test-resource-name",
+			},
+			Resources: []*domain.ResourceConfig{
+				{
+					Type: gcs.ResourceTypeBucket,
+					Roles: []*domain.Role{
+						{
+							ID:          "Storage Legacy Bucket Writer",
+							Name:        "Storage Legacy Bucket Writer",
+							Description: "Read access to buckets with object listing/creation/deletion",
+							Permissions: []interface{}{"roles/storage.legacyBucketWriter"},
+						},
+					},
+				},
+			},
+		}
+		a := &domain.Appeal{
+			Role: "Storage Legacy Bucket Writer",
+			Resource: &domain.Resource{
+				URN:          "test-bucket-name",
+				Name:         "test-bucket-name",
+				ProviderType: "gcs",
+				ProviderURN:  "test-resource-name",
+				Type:         "bucket",
+			},
+			ID:          "999",
+			ResourceID:  "999",
+			AccountType: expectedAccountType,
+			AccountID:   expectedAccountID,
+		}
+
+		actualError := p.RevokeAccess(pc, a)
+
+		assert.Error(t, actualError)
+	})
+
 	t.Run("should revoke the access to bucket resource and return nil error", func(t *testing.T) {
 
 		expectedAccountType := "user"
