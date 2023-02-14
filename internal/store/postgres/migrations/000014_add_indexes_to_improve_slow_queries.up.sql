4a09cb2a00d72363a4b1bbbd833e147e5b58f1da
Haveiss <to@haveiss.com>
2023-02-14 11:50:00 +0700

    chore: add index explaination

--- internal/store/postgres/migrations/000014_add_indexes_to_improve_slow_queries.up.sql
+++ internal/store/postgres/migrations/000014_add_indexes_to_improve_slow_queries.up.sql
@@ -1,7 +1,26 @@
+-- This index reducing query cost from 13997.45..13997 to 17.66..17.67
+-- SELECT ...
+-- FROM "appeals"
+--     LEFT JOIN "resources" "Resource" ON "appeals"."resource_id" = "Resource"."id"
+--     AND "Resource"."deleted_at" IS NULL
+--     LEFT JOIN "grants" "Grant" ON "appeals"."id" = "Grant"."appeal_id"
+--     AND "Grant"."deleted_at" IS NULL
+-- WHERE "appeals"."created_by" = '<creator>'
+--     AND "appeals"."status" IN (...)
+--     AND "appeals"."deleted_at" IS NULL
+-- ORDER BY ARRAY_POSITION(
+--         ARRAY [...],
+--         "appeals"."status"
+--     ),
+--     "updated_at" desc;
+
 CREATE INDEX IF NOT EXISTS idx_appeals_created_by_status_deleted_at_updated_at
 ON appeals (created_by, status, deleted_at, updated_at DESC)
 WHERE deleted_at IS NULL;
 
+-- This index reducing query cost from 0.15..8.17 to 0.00..1.02
+-- Since we have a lot of queries like this due to the gorm soft delete:
+-- SELECT * FROM <table> WHERE id = '<uuid>' AND "deleted_at" IS NULL ORDER BY "id" LIMIT 1
 CREATE INDEX IF NOT EXISTS idx_appeals_id_deleted_at_null
 ON appeals (id, deleted_at)
 WHERE deleted_at IS NULL;
