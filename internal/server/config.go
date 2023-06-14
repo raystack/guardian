9b4d61fa16db45a8b073618eabd805ec61e69bc6
Chief-Rishab <rishab.jain7209@gmail.com>
2023-06-14 12:38:46 +0530

    feat: inactive accounts revocation

--- internal/server/config.go
+++ internal/server/config.go
@@ -5,34 +5,12 @@ import (
 	"fmt"
 
 	"github.com/raystack/guardian/internal/store"
+	"github.com/raystack/guardian/jobs"
 	"github.com/raystack/guardian/pkg/auth"
 	"github.com/raystack/guardian/pkg/tracing"
 	"github.com/raystack/guardian/plugins/notifiers"
 	"github.com/raystack/salt/config"
 )
-
-type JobType string
-
-const (
-	FetchResources            JobType = "fetch_resources"
-	ExpiringGrantNotification JobType = "grant_expiration_reminder"
-	RevokeExpiredGrants       JobType = "grant_expiration_revocation"
-)
-
-type JobConfig struct {
-	JobType JobType
-	// Enabled is set as true for backward compatibility. If the job needs to be disabled, it must be present in the config with this value as false.
-	Enabled  bool   `mapstructure:"enabled" default:"true"`
-	Interval string `mapstructure:"interval"`
-}
-
-type Jobs struct {
-	FetchResources             JobConfig `mapstructure:"fetch_resources"`
-	RevokeExpiredGrants        JobConfig `mapstructure:"revoke_expired_grants"`
-	ExpiringGrantNotification  JobConfig `mapstructure:"expiring_grant_notification"`
-	RevokeExpiredAccess        JobConfig `mapstructure:"revoke_expired_access"`
-	ExpiringAccessNotification JobConfig `mapstructure:"expiring_access_notification"`
-}
 
 type DefaultAuth struct {
 	HeaderKey string `mapstructure:"header_key" default:"X-Auth-Email"`
@@ -42,6 +20,18 @@ type Auth struct {
 	Provider string        `mapstructure:"provider" default:"default"`
 	Default  DefaultAuth   `mapstructure:"default"`
 	OIDC     auth.OIDCAuth `mapstructure:"oidc"`
+}
+
+type Jobs struct {
+	FetchResources             jobs.Job `mapstructure:"fetch_resources"`
+	RevokeExpiredGrants        jobs.Job `mapstructure:"revoke_expired_grants"`
+	ExpiringGrantNotification  jobs.Job `mapstructure:"expiring_grant_notification"`
+	RevokeGrantsByUserCriteria jobs.Job `mapstructure:"revoke_grants_by_user_criteria"`
+
+	// Deprecated: use ExpiringGrantNotification instead
+	ExpiringAccessNotification jobs.Job `mapstructure:"expiring_access_notification"`
+	// Deprecated: use RevokeExpiredGrants instead
+	RevokeExpiredAccess jobs.Job `mapstructure:"revoke_expired_access"`
 }
 
 type Config struct {
